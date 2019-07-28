a2c53980b553d77686b64535c739d9d95f2544ec
Gil Birdsong <gilbert.b@witinnovation.com>
2019-07-28 14:44:41 -0500

    trying to fix cors

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const bodyParser = require('body-parser')
 const app = express()
 var port = process.env.PORT || 8080;
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
+
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
