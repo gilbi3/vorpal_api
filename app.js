5654f262b97c8b0cedf0a7d7e5ca1dd0f651615d
Gilbert Birdsong <gibirdso@microsoft.com>
2019-08-09 12:44:53 -0500

    cors change again

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ app.use(cors());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+    if (req.method == 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
+});
+
 var Note = require('./models/note');
 var Task = require('./models/task');
 var User = require('./models/user');
