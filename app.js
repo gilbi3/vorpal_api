3f041449e1b07dba53a81b22ae8ea38e7263c781
Gilbert Birdsong <gibirdso@microsoft.com>
2019-08-09 12:51:11 -0500

    testing

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 
     if (req.method == 'OPTIONS') {
