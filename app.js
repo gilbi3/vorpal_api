dfb2b8bf615d554c60b33d1625add83dd6e53158
Gilbert Birdsong <gibirdso@microsoft.com>
2019-08-09 12:52:25 -0500

    another test

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Origin', '*');
+	req.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 
